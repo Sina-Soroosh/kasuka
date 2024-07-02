8ff78419eb9f2ce874ba4f6d378ff3b8b38f8853
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-02 09:39:55 +0330

    coding about template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/modules/Header/Header";
+import About from "@/components/templates/About";
 import Hero from "@/components/templates/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
